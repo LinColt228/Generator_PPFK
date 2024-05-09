1070855bfe9873b37b9c4a0265fc8dd3ae82cc1b
LinColt228 <169100790+LinColt228@users.noreply.github.com>
2024-05-09 19:06:34 +0300

    Add files via upload

---
+++ localhost.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.1.2
+ */
+
+/**
+ * Database `my_user`
+ */
+
+/* `my_user`.`prepod` */
+$prepod = array(
+);
+/**
+ * Database `test1base`
+ */
+
+/* `test1base`.`users` */
+$users = array(
+);
